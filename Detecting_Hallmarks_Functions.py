c7447a9a590ac722acb39b987972a7f16c2ad812
mblacksmith1996 <blacksmi@umich.edu>
2023-11-05 13:42:19 -0500

    Re-added code sections relevant for detecting hallmarks from local assembly data

--- Detecting_Hallmarks_Functions.py
+++ Detecting_Hallmarks_Functions.py
@@ -58,9 +58,32 @@ def investigate_TSD_validity(flank_dist,dist,seq1,seq2,length,search_internal,in
     alignment_fail = []
 
     if search_internal == True:
-        sys.exit("search_internal will be enabled in a future update. Please set search_internal == False")
+        #sys.exit("search_internal will be enabled in a future update. Please set search_internal == False")
          
         #to be added at a later date for non-reference retro-intersetions
+         for seq in [seq1,seq2]:
+            if abs(flank_dist - int(seq[1])) > dist and abs(flank_dist - int(seq[3])) > dist:
+                if seq == seq1:
+                    alignment_fail.append("seq1")
+                else:
+                    alignment_fail.append("seq2")
+        if len(alignment_fail) >= 1:
+            print(f"At least 1 alignment failed to be withing {dist}bp of the start/end of the inserted sequence")
+            print(seq1,seq2)
+            return False, False
+        else:
+            if (int(seq1[1]) < 5) or (int(seq2[1]) < 5) or (flank_dist*2 - int(seq1[3]) < 5) or (flank_dist*2 - int(seq2[3]) < 5):
+                #print(length)
+                if flank_dist > length/2:
+                    False,False
+                    #sys.exit()
+                #print(seq1,seq2)
+                print("TSD may be longer than extracted flank. Doubling flank and trying again")
+                subprocess.run("rm water.txt",shell=True)
+                return True, False
+    else:
+        print(seq1,seq2)
+        print(dist,flank_dist)
     else:
         #sys.exit()
         if (int(seq1[1]) <= 5) or (flank_dist+interior_dist - int(seq2[3]) < 5):
@@ -92,8 +115,10 @@ def run_water(flank_dist, dist, coords, extraction_path,ref,orientation, search_
     re_run = True
     while re_run:
         if search_internal == True:
-            sys.exit("search_internal will be enabled in a future update. Please set search_internal == False")
+            #sys.exit("search_internal will be enabled in a future update. Please set search_internal == False")
             #to be added at a later date for non-reference retro-intersetions
+            extract_up = f"{coords[0]}:{max(coords[1]-flank_dist,1)}-{coords[1]+(flank_dist -1)}"
+            extract_down = f"{coords[0]}:{coords[2]-(flank_dist-1)}-{coords[2]+flank_dist}"
         elif search_internal != True:
             dist = flank_dist
             extract_up = f"{coords[0]}:{max(coords[1]-dist,1)}-{coords[1]+(interior_dist-1)}"
@@ -133,7 +158,275 @@ def run_water(flank_dist, dist, coords, extraction_path,ref,orientation, search_
                 print("Seq1 and Seq2 are identical")
             return seq1,seq2
             
+def Refine_Coords(seq1,seq2,extract):
+    if seq1 != "":
+        print(seq1,seq2,extract)
+        #print(int(seq1[0].split("-")[0])+int(seq1[3])+1,int(seq2[0].split("-")[0])-2+int(seq1[1]))
+        start_after_TSD = int(seq1[0].split("-")[0])+int(seq1[3])
+        end_before_TSD = int(seq2[0].split("-")[0])-1+int(seq1[1])
+        RM_coords = [extract[0],start_after_TSD,end_before_TSD]
+        TSDs = [extract[0],int(seq1[0].split("-")[0])+int(seq1[1])-1,int(seq1[0].split("-")[0])+int(seq1[3])-1,int(seq2[0].split("-")[0])+int(seq2[1])-1,int(seq2[0].split("-")[0])+int(seq2[3])-1]
+        #print(TSDs)
+    else:
+        RM_coords = extract
+        TSDs = []    
+    return(RM_coords,TSDs)
+    
+def Repeat_Positions(repeat_masker_output, RM_position):
+    #load in the relevant portions of the out file.
+    if repeat_masker_output.endswith(".bed"):
+        command = f"cat {repeat_masker_output} | grep -w '^{RM_position[0]}'"
+        print(command)
+        proc = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
+        rm_out,err = proc.communicate()
+        rm_out = rm_out.decode()
+        rm_out = rm_out.split("\n")
+        #print(rm_out)
 
+        start_index = 1
+        end_index = 2
+        offset = 1
+        ME_Type = 4
+        O = 8
+
+    else:
+        command = f"cat {repeat_masker_output}"
+        proc = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
+        rm_out,err = proc.communicate()
+        rm_out = rm_out.decode()
+        rm_out = rm_out.split("\n")
+        #print(rm_out)
+
+        start_index = 5
+        end_index = 6
+        offset = 0
+        ME_Type = 10
+        O = 8
+
+    print(f"RM for Repeat_Positions is {repeat_masker_output}")
+    #new method of determining % L1 content.
+    lower_bound = RM_position[1]
+    upper_bound = RM_position[2]
+
+    L1s = []
+    #with open(repeat_masker_output, 'rt') as file:
+    for line in rm_out:
+        if line.startswith("There were no repetitive sequences detected"):
+            break
+        if line.startswith("   SW") or line.startswith("score") or line == "\n" or line == "":
+            continue
+        line = line.split()
+        #Define relevant parameters
+        #print(line)
+        RM_start_in_seq = int(line[start_index])+ offset
+        RM_end_in_seq = int(line[end_index])
+        Element_Class = line[ME_Type]
+        Element_Orientation = line[O]
+
+        #create list containing the location of the sequence relative to the LINE-1
+        if repeat_masker_output.endswith(".bed") and Element_Orientation == "+":
+            orientation = "Forward"
+            Position_within_Repeat = [int(line[9].split(',')[0]),int(line[9].split(',')[1])]
+        elif repeat_masker_output.endswith(".bed") and Element_Orientation == "C":
+            Position_within_Repeat = [int(line[9].split(',')[2]),int(line[9].split(',')[1])]
+            orientation = "Reverse"
+        elif Element_Orientation == "+":
+            orientation = "Forward"
+            Position_within_Repeat = [int(line[11]),int(line[12])]
+        elif Element_Orientation == "C":
+            Position_within_Repeat = [int(line[13]),int(line[12])]
+            orientation = "Reverse"
+        line_start = 0
+        line_end = 0
+
+        if Element_Class == "LINE/L1":
+            #print(f"Lower and Upper Bounds of the detected insertion are {lower_bound} and {upper_bound}")
+            #print(f"Start and end of detected repeat are {RM_start_in_seq} {RM_end_in_seq}")
+            #print(line)
+            #if RM is outside of the insertion
+            if RM_end_in_seq < int(lower_bound):
+                #print("L1 outide insertion")
+                continue
+            elif RM_start_in_seq > int(upper_bound):
+                #print("L1 outide insertion")
+                break
+            #if RM extends beyond both ends of the insertion    
+            elif RM_start_in_seq < int(lower_bound) and RM_end_in_seq > int(upper_bound):
+                #print(line)
+                line_start = int(lower_bound)
+                line_end = int(upper_bound)
+                #print("L1 extends beyond insertion boundaries")
+
+            #if the RM covers one of the boundaries of the insertion
+            elif RM_start_in_seq <= int(lower_bound) and RM_end_in_seq <= int(upper_bound):
+                #print(line)
+                line_start = int(lower_bound)
+                line_end = RM_end_in_seq
+                #print("L1 overlaps one boundary")
+            elif RM_start_in_seq >= int(lower_bound) and RM_start_in_seq <= int(upper_bound) and RM_end_in_seq >= int(upper_bound):
+                #print(line)
+                line_start = RM_start_in_seq
+                line_end = int(upper_bound)
+                #print("L1 overlaps one boundary")
+
+            #if the RM is entirely within the insertion
+            elif RM_start_in_seq > int(lower_bound) and RM_start_in_seq < int(upper_bound) and RM_end_in_seq < int(upper_bound):
+                #print(line)
+                line_start = RM_start_in_seq
+                line_end = RM_end_in_seq
+                #print("L1 is within insertion boundary")
+            else:
+                print("Erroneous Read Detected")
+                print(line)
+                sys.exit("Erroneous RM Detected")
+            #print("Appending L1")
+            #print(line)
+            L1s.append([line_start, line_end, orientation, Position_within_Repeat])
+
+    #print(initial_range)
+    #print(L1s)
+
+
+    #combine nearby elements.
+    diff = 50
+    exit_loop = False
+    if len(L1s) == 0:
+        print("Length of L1s list is 0. Terminating")
+        sys.exit()
+    while not exit_loop:
+        #print('hi',len(L1s))
+        merged_L1 = []
+        for i in range(len(L1s)):
+            #print('hi')
+            if i+1 == len(L1s):
+                exit_loop = True
+                break            
+            #sys.exit()
+            if L1s[i][2] == L1s[i+1][2]:
+                #print(f"Orientations are the same",L1s[i][2],L1s[i+1][2])
+                #sys.exit()
+                if L1s[i][2] == "Forward" and abs(L1s[i+1][3][0] - L1s[i][3][1]) < diff:
+                    #print(f"L1s are within range")
+                    merged_L1 = [L1s[i][0],L1s[i+1][1],L1s[i][2],[L1s[i][3][0],L1s[i+1][3][1]]]
+                    #print(merged_L1)
+                elif L1s[i][2] == "Reverse" and abs(L1s[i][3][1] - L1s[i+1][3][0]) < diff:
+                    #print(f"L1s are within range",L1s[i][3][1],L1s[i+1][3][0],diff)
+                    merged_L1 = [L1s[i][0],L1s[i+1][1],L1s[i][2],[L1s[i][3][0],L1s[i+1][3][1]]]
+                    #print(merged_L1)
+                #else:
+                    #print(f"L1s are close in contig distance, but do not overlap based on LINE-1 coordinates. The distance between the elements is larger than {diff}")
+
+            else:
+                #print(f"Orientations are different",L1s[i][2],L1s[i+1][2])
+                #sys.exit()
+                if max(int(L1s[i][3][0]),int(L1s[i][3][1])) > max(int(L1s[i+1][3][0]),int(L1s[i+1][3][1])):
+                    #sys.exit()
+                    if L1s[i][2] == "Forward":
+                            #sys.exit()
+                            continue
+                    if abs(L1s[i][3][1] - L1s[i+1][3][1]) > diff:
+                        #print("L1s are too far apart. Moving on")
+                        continue
+                    else:
+                        #print("L1s are within range")
+                        merged_L1 = [L1s[i][0],L1s[i+1][1],L1s[i][2],[L1s[i][3][0],L1s[i+1][3][0]]]
+                        #print(merged_L1)
+                        #sys.exit()
+                    #sys.exit()
+                else:
+                    #diff2 = abs(max(L1s[i][0],L1s[i][1]) -  min(L1s[i+1][0],L1s[i+1][1]))
+                    if L1s[i][2] == "Forward":
+                        #sys.exit()
+                        continue
+                    if abs(L1s[i][3][0] - L1s[i+1][3][0]) > diff:
+                        #print("L1s are too far apart. Moving on")
+                        continue
+                    else:
+                        #print("L1s are within range")
+                        merged_L1 = [L1s[i][0],L1s[i+1][1],L1s[i][2],[L1s[i][3][1],L1s[i+1][3][1]]]
+                        #print(merged_L1)
+                        #sys.exit()
+            if merged_L1 != []:
+                L1s[i] = merged_L1
+                L1s.pop(i+1)
+                break
+                            #print(merged_L1)
+    #print(L1s)
+    #return L1s
+    #sys.exit()
+
+    #sys.exit()
+    #determine L1 content between TSDs
+    #print(f"L1s is {L1s}")
+    if len(L1s) > 1:
+        L1_pos = "["
+        L1_content = 0
+        orientations = []
+        for i in range(len(L1s)):
+            item = L1s[i]
+            if i > 0:
+                if item[0] <= L1s[i-1][1]:
+                    item[0] = L1s[i-1][1] + 1
+            orientations.append(item[2])
+            L1_pos = L1_pos + f"[{item[3][0]},{item[3][1]}]"
+            #print(f"{L1_content} + ({item[1]}-{item[0]})+1")
+            L1_content = L1_content + (item[1]-item[0])+1
+        L1_pos = L1_pos + "]"
+        print(L1_pos)
+        transduction = ["N/A","N/A"]
+        if len(set(orientations)) == 1:
+            orientation = orientations[0]
+        else:
+            orientation = "Multiple_L1_Orientations_detected"
+        if orientation == "Multiple_L1_Orientations_detected":
+            transduction = ["N/A","N/A"]
+        elif orientation == "Forward":
+            if int(L1s[-1][1]) - int(upper_bound) == 0:
+                transduction = ["N/A","N/A"]
+            else:
+                transduction = [L1s[-1][1],upper_bound]
+            #print(transduction)
+        else:
+            if int(lower_bound) - int(L1s[0][0]) == 0:
+                transduction = ["N/A","N/A"]
+            else:
+                transduction = [lower_bound,L1s[0][0]]
+            #print(transduction)
+
+    else:
+        if L1s == []:
+            L1_content = "No_L1_detected"
+            transduction = ["N/A","N/A"]
+            orientation = "No_L1_detected"
+            L1_pos = "No_L1_detected"
+            #print('goop')
+        else:
+            orientation = L1s[0][2]
+            L1_content = (L1s[0][1]-L1s[0][0])+1
+            L1_pos = f"[{L1s[0][3][0]},{L1s[0][3][1]}]"
+            print(L1_content)
+            if L1_content == 1:
+                L1_content = 0
+                transduction = ["N/A","N/A"]
+            else:
+                #putative 3' transduction detection
+                if orientation == "Forward":
+                    if int(L1s[0][1]) - int(upper_bound) == 0:
+                        transduction = ["N/A","N/A"]
+                    else:
+                        transduction = [L1s[0][1],upper_bound]
+                    #print(transduction)
+                else:
+                    if int(lower_bound) - int(L1s[0][0]) == 0:
+                        transduction = ["N/A","N/A"]
+                    else:
+                        transduction = [lower_bound,L1s[0][0]]
+                    #print(transduction)
+
+    print(L1s)
+    #if len(L1s) > 1:
+    #sys.exit()
+    return L1_content, transduction, orientation, L1_pos, L1s
 def extract_for_Poly_A(transduction,extraction_path,extracted_seq,extra_seq,orientation,internal,interior_dist,exterior_cutoff=0):
     """
     Creates the samtools faidx command which will extract sequence for poly(A) detection.
@@ -154,8 +447,23 @@ def extract_for_Poly_A(transduction,extraction_path,extracted_seq,extra_seq,orie
     """
     
     if internal == True:
-        sys.exit("internal == True will be enabled in a future update. Please set internal == False")        
-        #to be added at a later date for non-reference retro-intersetions
+        #sys.exit("internal == True will be enabled in a future update. Please set internal == False")        
+        if transduction[0] != "N/A":
+            if orientation == "Reverse":
+                #extract from end of TSD to the LINE-1
+                faidx_cmd = f"samtools faidx {extraction_path} {extracted_seq[0]}:{transduction[0]}-{min(int(transduction[1])+extra_seq,extracted_seq[2])}"
+                start_in_contig = transduction[0]
+            elif orientation == "Forward":
+                faidx_cmd = f"samtools faidx {extraction_path} {extracted_seq[0]}:{max(int(transduction[0])-extra_seq,extracted_seq[1])}-{int(transduction[1])}"
+                start_in_contig = max(int(transduction[0])-extra_seq,extracted_seq[1])
+        else:
+            #don't forget to filter to not go earlier than the start of the element.
+            if orientation == "Forward":
+                faidx_cmd = f"samtools faidx {extraction_path} {extracted_seq[0]}:{max(extracted_seq[2]-extra_seq,extracted_seq[1])}-{extracted_seq[2]}"
+                start_in_contig = max(extracted_seq[2]-extra_seq,extracted_seq[1])
+            elif orientation == "Reverse":
+                faidx_cmd = f"samtools faidx {extraction_path} {extracted_seq[0]}:{extracted_seq[1]}-{min(extracted_seq[1]+extra_seq,extracted_seq[2])}"
+                start_in_contig = extracted_seq[1]#to be added at a later date for non-reference retro-intersetions
     elif internal == False:
         if exterior_cutoff == 0 and orientation == "Forward":
             exterior_cutoff = 100000000000
@@ -316,6 +624,14 @@ def Detect_Poly_As(transduction,extraction_path,extracted_seq,extra_seq,max_dist
                         if (extracted_seq[2] + extra_seq) - poly[2] < 5:
                             terminated_early = True
                 else:
+                    continue
                     sys.exit()
             print(final_polys)
     return final_polys, terminated_early
+    
+def transduction_detection(poly_a_list):
+    if len(poly_a_list) < 2:
+        return ["",""]
+    else:
+        Transduction = [poly_a_list[0][2]+1,poly_a_list[-1][0]-1]
+        return Transduction
